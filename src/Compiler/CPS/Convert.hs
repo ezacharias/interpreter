9295dd765e0b6636c7198bf314defb9c48070aee
Edwin Zacharias <ezacharias@gmail.com>
2014-04-21 21:11:59 +0000

    Now passes types in CPS conversion.

--- src/Compiler/CPS/Convert.hs
+++ src/Compiler/CPS/Convert.hs
@@ -7,20 +7,20 @@ convertTerm :: Simple.Term -> H -> K -> M CPS.Term
 convertTerm t h k =
   case t of
     Simple.ApplyTerm t1 t2 ->
-      convertTerm t1 h $ \ h d1 ->
-        convertTerm t2 h $ \ h d2 ->
-          createKClosure k $ \ d3 ->
+      convertTerm t1 h $ \ h d1 ty1 ->
+        convertTerm t2 h $ \ h d2 _ ->
+          createKClosure k (arrowTypeResult ty1) $ \ d3 _ ->
             createHClosure h $ \ d4 ->
               return $ CPS.ApplyTerm d1 [d2, d3, d4]
     Simple.CaseTerm t1 c1s -> do
-      convertTerm t1 h $ \ h d1 -> do
-        createKClosure k $ \ d3 ->
+      convertTerm t1 h $ \ h d1 _ -> do
+        createKClosure k undefined $ \ d3 _ ->
           createHClosure h $ \ d4 -> do
             c1s' <- mapM (convertRule (createH d4) (createK d3)) c1s
             return $ CPS.CaseTerm d1 c1s'
     Simple.CatchTerm d1 d2 t1 ->
       let h' d3 =
-            createKClosure k $ \ d6 ->
+            createKClosure k undefined $ \ d6 _ ->
               createHClosure h $ \ d7 -> do
                 c1s <- getStandardRules
                 i   <- getNormalResultIndex undefined
@@ -36,21 +36,21 @@ convertTerm t h k =
                                           $ CPS.ApplyTerm d6 [d6, d7])
                 c1s <- return $ substitute i c c1s
                 return $ CPS.CaseTerm d3 c1s
-          k' h d3 = do
+          k' h d3 ty3 = do
             d4  <- gen
             d5  <- getResultTypeIdent
-            i   <- getNormalResultIndex undefined
+            i   <- getNormalResultIndex ty3
             t2' <- h d4
             return $ CPS.ConstructorTerm d4 d5 i [d3] t2'
        in convertTerm t1 h' k'
     -- Should we do something to make this better?
     Simple.FunTerm d1 -> do
       d2  <- gen
-      t1' <- k h d2
+      ty1 <- getFunType d1
+      t1' <- k h d2 ty1
       d3  <- gen
       d4  <- gen
       d5  <- getResultTypeIdent
-      ty1 <- getFunType d1
       ty2s <- return $ [ CPS.ArrowType [ty1, CPS.ArrowType [CPS.SumType d5]]
                        , CPS.ArrowType [CPS.SumType d5]
                        ]
@@ -60,11 +60,14 @@ convertTerm t h k =
       d3   <- gen
       d4   <- gen
       t1'  <- convertTerm t1 (createH d4) (createK d3)
-      t2'  <- k h d2
       ty1' <- convertType ty1
+      d5   <- getResultTypeIdent
+      t2'  <- k h d2 (CPS.ArrowType [ ty1'
+                                    , CPS.ArrowType [undefined, CPS.ArrowType [CPS.SumType d5]]
+                                    , CPS.ArrowType [CPS.SumType d5]
+                                    ])
       -- I am not sure how I want to get this type.
       ty3' <- undefined
-      d5   <- getResultTypeIdent
       ty2s <- return $ [ ty1'
                        , CPS.ArrowType [ty3', CPS.ArrowType [CPS.SumType d5]]
                        , CPS.ArrowType [CPS.SumType d5]
@@ -72,11 +75,12 @@ convertTerm t h k =
       return $ CPS.LambdaTerm d2 [d1, d3, d4] ty2s t1' t2'
     Simple.StringTerm s1 -> do
       d1 <- gen
-      t1 <- k h d1
+      t1 <- k h d1 CPS.StringType
       return $ CPS.StringTerm d1 s1 t1
     Simple.ThrowTerm d1 t1 ->
-      convertTerm t1 h $ \ h d1 ->
-        createKClosure k $ \ d4 -> do
+      convertTerm t1 h $ \ h d1 ty1 -> do
+        ty2 <- getTagResultType d1
+        createKClosure k ty2 $ \ d4 _ -> do
           d2 <- gen
           d3 <- getResultTypeIdent
           i <- getThrowIndex d1
@@ -95,6 +99,9 @@ convertTerm t h k =
  | VariableTerm Ident
 -}
 
+getTagResultType :: CPS.Ident -> M CPS.Type
+getTagResultType d = undefined
+
 convertType :: Simple.Type -> M CPS.Type
 convertType = undefined
 
@@ -111,6 +118,11 @@ getNormalResultIndex ty = undefined
 getResultTypeIdent :: M CPS.Ident
 getResultTypeIdent = undefined
 
+getHandlerType :: M CPS.Type
+getHandlerType = do
+  d <- getResultTypeIdent
+  return $ CPS.ArrowType [CPS.SumType d]
+
 -- Returns the constructor index for the tag.
 getThrowIndex :: Simple.Ident -> M CPS.Index
 getThrowIndex d = undefined
@@ -119,7 +131,7 @@ createH :: CPS.Ident -> H
 createH d1 d2 = return $ CPS.ApplyTerm d1 [d2]
 
 createK :: CPS.Ident -> K
-createK d1 h d2 =
+createK d1 h d2 _ =
   createHClosure h $ \ d3 ->
     return $ CPS.ApplyTerm d1 [d2, d3]
 
@@ -130,24 +142,23 @@ convertRule h k (d1s, t1) = do
 
 type M a = [a]
 
-type K = H -> CPS.Ident -> M CPS.Term
+type K = H -> CPS.Ident -> CPS.Type -> M CPS.Term
 type H = CPS.Ident -> M CPS.Term
 
 -- Eta-reduce the K closure if possibe.
-createKClosure :: K -> (CPS.Ident -> M CPS.Term) -> M CPS.Term
-createKClosure k m = do
+createKClosure :: K -> CPS.Type -> (CPS.Ident -> CPS.Type -> M CPS.Term) -> M CPS.Term
+createKClosure k ty1 m = do
   d1 <- gen
   d2 <- gen
-  t1 <- k (\ d3 -> return $ CPS.ApplyTerm d2 [d3]) d1
-  ty1 <- undefined
+  t1 <- k (\ d3 -> return $ CPS.ApplyTerm d2 [d3]) d1 ty1
+  ty2 <- getHandlerType
   case t1 of
     CPS.ApplyTerm d3 [d4, d5] | d3 /= d4 && d3 /= d5 && d1 == d4 && d2 == d5 ->
-      m d3
+      m d3 (CPS.ArrowType [ty1, ty2])
     _ -> do
       d3 <- gen
-      t2 <- m d3
-      d4 <- getResultTypeIdent
-      return $ CPS.LambdaTerm d3 [d1, d2] [ty1, CPS.ArrowType [CPS.SumType d4]] t1 t2
+      t2 <- m d3 (CPS.ArrowType [ty1, ty2])
+      return $ CPS.LambdaTerm d3 [d1, d2] [ty1, ty2] t1 t2
 
 -- Eta-reduce the H closure if possibe.
 createHClosure :: H -> (CPS.Ident -> M CPS.Term) -> M CPS.Term
@@ -166,7 +177,11 @@ createHClosure h m = do
 gen :: M CPS.Ident
 gen = undefined
 
+arrowTypeResult :: CPS.Type -> CPS.Type
+arrowTypeResult (CPS.ArrowType [_, CPS.ArrowType [ty, _], _]) = ty
+arrowTypeResult _ = error "arrowTypeResult"
+
 substitute :: Int -> a -> [a] -> [a]
-substitute 0 x (_:ys) = x:ys
-substitute n x (y:ys) = y : substitute (n-1) x ys
-substitute n x []     = error "substitute out of bounds"
+substitute 0 x (_ : ys) = x : ys
+substitute n x (y : ys) = y : substitute (n-1) x ys
+substitute n x []       = error "substitute out of bounds"
