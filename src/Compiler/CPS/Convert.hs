ce6993b801b798b4a363e96c9614cd10937ec651
Edwin Zacharias <ezacharias@gmail.com>
2014-04-23 19:05:16 +0000

    Review of everything except h is catch for CPS conversion.

--- src/Compiler/CPS/Convert.hs
+++ src/Compiler/CPS/Convert.hs
@@ -25,7 +25,11 @@ convertTerm t h k =
           createHClosure h $ \ d4 -> do
             c1s' <- mapM (convertRule (createH d4) (createK d3)) c1s
             return $ CPS.CaseTerm d1 c1s'
+
     Simple.CatchTerm d1 d2 t1 ->
+      -- We have to begin here, which handles the catch. I think it should
+      -- very nearly simply call the function with h, k, and the result, which
+      -- would be easy.
       let h' d3 = do
             -- The type given to K is the stream type, which I suppose we should calculate using the given types.
             (ty1, ty2) <- undefined
@@ -49,33 +53,37 @@ convertTerm t h k =
                 return $ CPS.CaseTerm d3 c1s
           k' h d3 ty3 = do
             d4  <- gen
+            -- This I think could be a normal result to a particular tag, but
+            -- I'm not sure we want to do that.
             d5  <- getResultTypeIdent
             i   <- getNormalResultIndex ty3
             t2' <- h d4
-            return $ CPS.ConstructorTerm d4 d5 i [d3] t2'
+            return $ CPS.ConstructorTerm d4 d5 i [d3]
+                   $ t2'
        in convertTerm t1 h' k'
+
     -- Should we do something to make this better?
     Simple.FunTerm d1 -> do
       d2  <- gen
       ty1 <- getFunType d1
-      t1' <- k h d2 ty1
+      t1  <- k h d2 ty1
       d3  <- gen
       d4  <- gen
-      d5  <- getResultTypeIdent
-      ty2s <- return $ [ CPS.ArrowType [ty1, CPS.ArrowType [CPS.SumType d5]]
-                       , CPS.ArrowType [CPS.SumType d5]
-                       ]
-      return $ CPS.LambdaTerm d2 [d3, d4] ty2s (CPS.CallTerm d1 [d3, d4]) $ t1'
+      ty0 <- getHandlerType
+      return $ CPS.LambdaTerm d2 [d3, d4] [CPS.ArrowType [ty1, ty0], ty0] (CPS.CallTerm d1 [d3, d4])
+             $ t1
     Simple.LambdaTerm d1 ty1 t1 -> do
-      d2   <- gen
-      d3   <- gen
-      d4   <- gen
-      t1'  <- convertTerm t1 (createH d4) (createK d3)
+      d1' <- gen
       ty1' <- convertType ty1
-      ty2' <- undefined -- should be in the lambda
-      ty0' <- getHandlerType
-      t2'  <- k h d2 (CPS.ArrowType [ty1', CPS.ArrowType [ty2', ty0'], ty0'])
-      return $ CPS.LambdaTerm d2 [d1, d3, d4] [ty1', CPS.ArrowType [ty2', ty0'], ty0'] t1' t2'
+      bind d1 d1' ty1' $ do
+        d2   <- gen
+        d3   <- gen
+        d4   <- gen
+        t1'  <- convertTerm t1 (createH d4) (createK d3)
+        ty2' <- undefined -- should be in the lambda
+        ty0' <- getHandlerType
+        t2'  <- k h d2 (CPS.ArrowType [ty1', CPS.ArrowType [ty2', ty0'], ty0'])
+        return $ CPS.LambdaTerm d2 [d1', d3, d4] [ty1', CPS.ArrowType [ty2', ty0'], ty0'] t1' t2'
     Simple.StringTerm s1 -> do
       d1 <- gen
       t1 <- k h d1 CPS.StringType
@@ -83,12 +91,13 @@ convertTerm t h k =
     Simple.ThrowTerm d1 t1 ->
       convertTerm t1 h $ \ h d1 ty1 -> do
         ty2 <- getTagResultType d1
-        createKClosure k ty2 $ \ d4 _ -> do
-          d2 <- gen
-          d3 <- getResultTypeIdent
+        createKClosure k ty2 $ \ d2 _ -> do
+          d3 <- gen
+          d4 <- getResultTypeIdent
           i <- getThrowIndex d1
-          t2' <- h d2
-          return $ CPS.ConstructorTerm d2 d3 i [d1, d4] t2'
+          t2 <- h d3
+          return $ CPS.ConstructorTerm d3 d4 i [d1, d2]
+                 $ t2
     Simple.UnreachableTerm _ -> do
       -- This should not take a type.
       return $ CPS.UnreachableTerm
@@ -232,6 +241,8 @@ getThrowIndex d = undefined
 getFunType :: Simple.Ident -> M CPS.Type
 getFunType = undefined
 
+-- The first set is every possible normal result type. The second set is every
+-- possible tag result.
 getStandardRules :: M [([CPS.Ident], CPS.Term)]
 getStandardRules = undefined
 
@@ -243,6 +254,44 @@ getTagResultType d = undefined
 
 getStreamType :: CPS.Type -> CPS.Type -> CPS.Type -> M CPS.Type
 getStreamType = undefined
+
+makeNormalResult :: CPS.Type -> H -> K -> M ([CPS.Ident], CPS.Term)
+makeNormalResult ty1 h k = do
+  d1 <- gen
+  return ([d1], CPS.UnreachableTerm)
+
+makeTagResult :: CPS.Ident -> CPS.Type -> CPS.Ident -> CPS.Ident -> M ([CPS.Ident], CPS.Term)
+makeTagResult fd ty1 hd kd = do
+  d1 <- gen
+  d2 <- gen
+  d3 <- gen
+  d4 <- gen
+  d5 <- gen
+  d6 <- gen
+  d7 <- gen
+  d8 <- gen
+  i  <- gen
+  return ( [d1, d2]
+         , CPS.LambdaTerm d3 [d4, d5] [ty1, undefined]
+             ( CPS.LambdaTerm d6 [d7, d8] [undefined, undefined]
+                 ( CPS.CallTerm fd [d7, kd, d8] )
+             $ CPS.ApplyTerm d2 [d4, d6]
+             )
+         $ CPS.ConstructorTerm d7 d8 i [d1, d3]
+         $ CPS.ApplyTerm hd [d7]
+         )
+
+exportRecursiveFoo :: CPS.Ident -> CPS.Ident -> CPS.Ident -> M CPS.Ident
+exportRecursiveFoo d1 kd hd = do
+  d0 <- gen
+  exportFun d0 $ CPS.Fun [d1, kd, hd] [] $
+                   undefined
+  undefined
+
+exportFun :: CPS.Ident -> CPS.Fun -> M ()
+exportFun = undefined
+
+
 
 
 -- Utility Functions
